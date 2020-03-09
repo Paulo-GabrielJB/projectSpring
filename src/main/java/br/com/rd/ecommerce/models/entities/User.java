706284fbe284a4427713a0e443dd2a61fb94e367
Paulo Gabriel <pgj1085@gmail.com>
2020-03-09 14:52:52 -0300

    Usando marcadores pros geters, setters e ctor

--- src/main/java/br/com/rd/ecommerce/models/entities/User.java
+++ src/main/java/br/com/rd/ecommerce/models/entities/User.java
@@ -1,47 +1,17 @@
 package br.com.rd.ecommerce.models.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
     private Long id;
     private String firstName;
     private String lastName;
     private Integer age;
 
-    public User(){
-    }
-
-    public User(Long id, String firstName, String lastName, Integer age) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
